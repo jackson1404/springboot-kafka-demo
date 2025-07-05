4ab1efb96e94d688f9d4452301ab5e6acd734f98
jackson1404 <jackson14042004@gmail.com>
2025-07-05 23:43:22 +0630

    added dynamic config

--- src/main/java/com/jackson/apache_kafka/testing/config/consumer/MessageConsumer.java
+++ src/main/java/com/jackson/apache_kafka/testing/config/consumer/MessageConsumer.java
@@ -1,12 +1,21 @@
 package com.jackson.apache_kafka.testing.config.consumer;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class MessageConsumer {
 
-    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    @Value("${spring.kafka.topics.testing-event}")
+    private String topicName;
+
+    @Value("${spring.kafka.groups.testing-group}")
+    private String groupId;
+
+    @KafkaListener(topics = "#{__listener.topicName}", groupId = "#{__listener.groupId}")
     public void listen(String message){
         System.out.println("Received message" + message);
     }
--- src/main/java/com/jackson/apache_kafka/testing/controller/KafkaController.java
+++ src/main/java/com/jackson/apache_kafka/testing/controller/KafkaController.java
@@ -1,5 +1,6 @@
 package com.jackson.apache_kafka.testing.controller;
 
+import com.jackson.apache_kafka.testing.config.consumer.MessageConsumer;
 import com.jackson.apache_kafka.testing.config.producer.MessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +13,12 @@ public class KafkaController {
     @Autowired
     private MessageProducer messageProducer;
 
+    @Autowired
+    private MessageConsumer messageConsumer;
+
     @PostMapping("/sendMessage")
     public String sendMessage(@RequestParam("message") String message){
-        messageProducer.sendMessage("my-topic", message);
+        messageProducer.sendMessage(messageConsumer.getTopicName(), message);
         return "Message sent : " + message;
     }
 
